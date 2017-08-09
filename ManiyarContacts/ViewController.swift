ac2973a44af2c62e9c22b5dd32cc9811212016e6
Shadi Ghelman <shadi@Shadis-MacBook-Pro.local>
2017-08-09 13:19:01 +0430

    Initial Commit

---
+++ ManiyarContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManiyarContacts
+//
+//  Created by Shadi Ghelman on 8/9/17.
+//  Copyright © 2017 Shadi Ghelman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
